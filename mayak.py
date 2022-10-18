f09cc0f2cec6f850bcc6dd53850378ea414467f7
Axxolotl <103041459+Axxolotl@users.noreply.github.com>
2022-10-18 17:37:14 +0300

    Update mayak.py

--- mayak.py
+++ mayak.py
@@ -1,1 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
 
+import pandas as pd
+
+url = 'https://www.mayak-agent.ru/actresses'
+req = requests.get(url)
+
+soup = BeautifulSoup(req.text, 'lxml')
+
+actresses = soup.find(class_ = 'row bottom20px').find_all('a')
+hrefs = ['https://www.mayak-agent.ru' + i.get('href') for i in actresses]
