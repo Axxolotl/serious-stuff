1a8864fcc65f58b854064a3900c9436c47d30504
Axxolotl <103041459+Axxolotl@users.noreply.github.com>
2022-10-19 14:42:15 +0300

    Update mayak.py

--- mayak.py
+++ mayak.py
@@ -1,8 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+import re
 import pandas as pd
 
+from get_infofunc import get_personal_info
+
+cal = ['Tilda UID', 'Brand', 'SKU', 'Mark', 'Category', 'Title', 'Description', 'Text', 'Photo', 'Price',
+       'Quantity','Price Old','Editions','Modifications','External ID','Parent UID','Weight',
+       'Length','Width','Height','Url']
+
+dataframe = pd.DataFrame(columns=cal)
 url = 'https://www.mayak-agent.ru/actresses'
 req = requests.get(url)
 
@@ -11,21 +19,14 @@ soup = BeautifulSoup(req.text, 'lxml')
 actresses = soup.find(class_ = 'row bottom20px').find_all('a')
 hrefs = ['https://www.mayak-agent.ru' + i.get('href') for i in actresses]
 
-
-class_names = ['personal-wrapper', 'details-wrapper']
-nums_names = ['6', '12']
-personal_info = ''
-for i,j in zip(class_names, nums_names):
+for link in hrefs:
+    woman = requests.get(link)
+    w_soup = BeautifulSoup(woman.text, 'lxml')
     
-    personal = w_soup.find(class_ = i).find(class_ = 'row').find_all(class_ = f'col-xs-{j} col-md-8')
-    personal = [re.sub(r'[\n|\r|\t]', ' ', i.text).strip() for i in personal]
-
-    labels = w_soup.find(class_ = i).find(class_ = 'row').find_all(class_ = f'col-xs-{j} col-md-4 labels')
-    labels = [re.sub(r'[\n|\r|\t]', '', i.text).strip() for i in labels]
+    photos = [i.find('img')['src'] for i in w_soup.find_all(class_='swiper-slide')]
+    name = w_soup.find('h1').text.strip()
+    pers_info = get_personal_info(w_soup)
     
-    if i == 'personal-wrapper':
-        personal_info += '<br />'.join([i + ' ' + j for i,j in zip(labels, personal)]) + '<br />___________________________________________________________________<br />'
-    else:
-        personal_info += '<br /><br />'.join([i + ' ' + j for i,j in zip(labels, personal)]).replace('     ', '<br /><br />')
-        
-ssl = a_soup.find(class_='details-wrapper').find(class_='col-xs-12 links').find(class_='row').find(class_='col-xs-12 col-md-8').find_all('a')
+    dataframe.loc[len(dataframe.index)-1] = ['', '', '', '', '', name.upper(), '', pers_info, ' '.join(photos), '', '', '', '', '', '','','','','','','']
+
+dataframe.to_csv('store.csv')
