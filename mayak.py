e96933c5a2b333705ba2eef04f03d67d59b6fb32
Axxolotl <103041459+Axxolotl@users.noreply.github.com>
2022-10-19 14:05:29 +0300

    Update mayak.py

--- mayak.py
+++ mayak.py
@@ -12,11 +12,20 @@ actresses = soup.find(class_ = 'row bottom20px').find_all('a')
 hrefs = ['https://www.mayak-agent.ru' + i.get('href') for i in actresses]
 
 
-personal = w_soup.find(class_ = 'details-wrapper').find(class_ = 'row').find_all(class_ = 'col-xs-12 col-md-8')
-personal = [re.sub(r'[\n|\r|\t]', ' ', i.text).strip() for i in personal]
+class_names = ['personal-wrapper', 'details-wrapper']
+nums_names = ['6', '12']
+personal_info = ''
+for i,j in zip(class_names, nums_names):
+    
+    personal = w_soup.find(class_ = i).find(class_ = 'row').find_all(class_ = f'col-xs-{j} col-md-8')
+    personal = [re.sub(r'[\n|\r|\t]', ' ', i.text).strip() for i in personal]
 
-labels = w_soup.find(class_ = 'details-wrapper').find(class_ = 'row').find_all(class_ = 'col-xs-12 col-md-4 labels')
-labels = [re.sub(r'[\n|\r|\t]', '', i.text).strip() for i in labels]
-
-personal_info = '<br /><br />'.join([i + ' ' + j for i,j in zip(labels, personal)]).replace('     ', '<br /><br />')
+    labels = w_soup.find(class_ = i).find(class_ = 'row').find_all(class_ = f'col-xs-{j} col-md-4 labels')
+    labels = [re.sub(r'[\n|\r|\t]', '', i.text).strip() for i in labels]
+    
+    if i == 'personal-wrapper':
+        personal_info += '<br />'.join([i + ' ' + j for i,j in zip(labels, personal)]) + '<br />___________________________________________________________________<br />'
+    else:
+        personal_info += '<br /><br />'.join([i + ' ' + j for i,j in zip(labels, personal)]).replace('     ', '<br /><br />')
+        
 personal_info
