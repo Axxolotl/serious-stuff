c5e483d451b4270407ba6b805019006562149a73
be nice to me <46555349+AlcoholicAndMoron@users.noreply.github.com>
2022-10-20 19:44:49 +0300

    Update bot_rsuh.py

--- bot_rsuh.py
+++ bot_rsuh.py
@@ -29,7 +29,12 @@ def enter_info(message):
         bot.send_message(message.chat.id, text='\n'.join([str(i) + '. ' + j for i,j in dict_form.items()]))
         bot.send_message(message.chat.id, text='Введите цифру, соответствующую Вашей форме')
         bot.register_next_step_handler(message, get_form)
-
+     elif message.text == 'Ввести курс':
+        bot.send_message(message.chat.id, "Отправь мне цифру своего курса")
+            bot.register_next_step_handler(message, user_faculty)
+     elif message.text == 'Ввести название специальности':
+        bot.send_message(message.chat.id, 'Отправь мне свою специальность')
+        bot.register_next_step_handler(message, user_speciality)
         
         
         
@@ -37,3 +42,12 @@ def enter_info(message):
     def get_form(message):
         form_edu = message.text
         bot.send_message(message.chat.id, text=form_edu)
+        
+    def user_faculty(message):
+        if int(message.text) in range(1,7): 
+            faculty = message.text
+            bot.send_message(message.chat.id, faculty)
+       
+    def user_speciality(message):
+        speciality = message.text
+        bot.send_message(message.chat.id, speciality)
