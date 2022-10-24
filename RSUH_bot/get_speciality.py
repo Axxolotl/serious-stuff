d94b7a46cef809a63a701976ff61f90f887f7925
be nice to me <46555349+AlcoholicAndMoron@users.noreply.github.com>
2022-10-24 18:53:44 +0300

    Update get_speciality.py

--- RSUH_bot/get_speciality.py
+++ RSUH_bot/get_speciality.py
@@ -1,4 +1,3 @@
-# судя по всему, для парсинга расписания нам понадобиться доставать инфу через selenium
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
@@ -6,6 +5,7 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.options import Options
 
 import time
+import json
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -16,9 +16,16 @@ url = 'https://www.rsuh.ru/raspis/'
 driver.get(url)
 time.sleep(2)
 
-form =  ["Дневная", "Вечерняя","Заочная","Второе образование", "Магистратура","Аспирантура", "Дистанционное"]
-course = ['Курс ' + str(i) for i in range(1,7)]
+req = requests.get(url)
+soup = BeautifulSoup(req.text, 'lxml')
 
+form = [i.text.strip() for i in soup.find(attrs = {'id': 'formob'})]
+form.sort()
+del form[0:8]
+course = [i.text.strip() for i in soup.find(attrs = {'id':'kyrs'})]
+course.sort()
+del course[0:7]
+specialities = {}
 
 for edu_form in form:
     for course_num in course:
@@ -26,14 +33,9 @@ for edu_form in form:
         for i in zip(['formob', 'kyrs'], [edu_form, course_num]):
             select = Select(driver.find_element(By.ID, i[0]))
             select.select_by_visible_text(i[1])
-    
         # жмякаем на кнопку "продолжить"
         continue_ = driver.find_element(By.ID, 'filters').click()
         time.sleep(2)
-
-for i in zip(['formob', 'kyrs'], [form[0], course[0]]):
-    select = Select(driver.find_element(By.ID, i[0]))
-    select.select_by_visible_text(i[1])
-    
-continue_ = driver.find_element(By.ID, 'filters').click()
-time.sleep(2)
+        soup = BeautifulSoup(driver.find_element(By.ID, 'caf').get_attribute('innerHTML'), 'lxml')
+        groups = [i.text.strip() for i in soup.find_all('option')]
+        specialities[edu_form, course_num] = groups
