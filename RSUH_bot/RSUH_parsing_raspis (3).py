e3696fb474cee729a289428bf1fa0ec302604c70
Axxolotl <103041459+Axxolotl@users.noreply.github.com>
2022-11-06 18:10:32 +0300

    Update RSUH_parsing_raspis (3).py

--- RSUH_bot/RSUH_parsing_raspis (3).py
+++ RSUH_bot/RSUH_parsing_raspis (3).py
@@ -1,39 +1,29 @@
-#!/usr/bin/env python
-# coding: utf-8
+# библиотеки для парсинга данных
+import requests
+from bs4 import BeautifulSoup
 
-# In[62]:
-
-
-# я хочу загрузить полученные данные в pandas.DataFrame. В таблицу то есть.
-import pandas as pd
-
+# библиотека для работы со временем и датами (понадобится в дальнейшем)
 import datetime
 
-import requests
-
-from bs4 import BeautifulSoup
+# инфу будем хранить в джейсоновском формате (а зачем???)
 import json
 
-
-# In[63]:
-
-
+# считываем файл со специальностями (нам оттуда нужны значения кафедры)
 with open('специальности.json', 'r', encoding='utf-8') as file:
     specialities_json = json.load(file)
 
-
-# In[75]:
-
-
+######################################################### ПОДГОТОВКА К ПАРСИНГУ ################################################################### 
+# данные будем парсить через пост запросы, а пост запросам нужны данные
+# функция для получения данных в нужном формате
 def create_data(user_string):
     
-    def is_date_good(time, month=False):
-        if month and int(time)>12:
-            time = '1'
+    # функция для проверки корректности даты (просто на сайте требуется двузначное число)
+    def is_date_good(time):
         if len(time)<2:
             return '0'+time
         return time
     
+    # словарь с формой обучения, потому что она по-особенному фиксируется в запросе
     form_data = {
     'Дневная': "Д",
     "Вечерняя" : "В",
@@ -44,11 +34,15 @@ def create_data(user_string):
     "Дистанционное" : "У"
     }
     
+    # берем сегодняшнюю дату (она же начальная)
     today_date = datetime.datetime.now().date()
+    # считаем конечную дату
+    date_needed = today_date + datetime.timedelta(month=1)
     
-    date_needed = today_date + datetime.timedelta(days=7)
+    # опять создаем ключ для обращения к джейсону со специальностями
     key = user_string['form'] + ',' + user_string['course']
     
+    # создаем данные для пост запроса
     data = {
         'formob' : form_data[user_string['form']],
         'kyrs' : user_string['course'].split()[1],
@@ -56,50 +50,32 @@ def create_data(user_string):
         'caf' : specialities_json[key][user_string['speciality']],
         'cafzn' : user_string['speciality'],
         'sdate_year' : str(today_date.year),
-        'sdate_month': is_date_good(str(today_date.month),month=True),
+        'sdate_month': is_date_good(str(today_date.month)),
         'sdate_day' : is_date_good(str(today_date.day)),
         'fdate_year' : str(date_needed.year),
-        'fdate_month': is_date_good(str(int(today_date.month)+1), month=True),
-        'fdate_day' : is_date_good(str(today_date.day))
+        'fdate_month': is_date_good(str(date_needed.month)),
+        'fdate_day' : is_date_good(str(date_needed.day))
     }
     return data
 
-
-# In[76]:
-
-
-create_data({"form": "Дневная",
-    "course": "Курс 4",
-    "speciality": "ОИС_ИС (Группа: 1)"})
-
-
-# In[88]:
-
-
+########################################################## ПАРСЕР ДАННЫХ С САЙТА ##########################################################
 def parse_rsuh(data):
+    # ссылка 
     url = 'https://www.rsuh.ru/rasp/3.php'
     
+    # фигашим пост запрос и парсим его
     r = requests.post(url=url, data=data)
     soup = BeautifulSoup(r.text, 'html.parser')
     
+    # ищем всю инфу по расиписанию
     info = soup.findAll('tr')
     parse_data = []
     
+    # запихиваем всю инфу в список для дальнейшей обработки
     for i in info:
         parse_data.append([j.text for j in i.find_all('td')])
     return parse_data
-
-
-# In[89]:
-
-
-parse_rsuh(create_data({"form": "Дневная",
-    "course": "Курс 4",
-    "speciality": "ОИС_ИС (Группа: 1)"}))
-
-
-# In[34]:
-
+#############################################################################################################################################
 
 # def create_table(data):
 #     # создаем датафрейм с расписанием, в качестве названий колонок берем первую строку 
