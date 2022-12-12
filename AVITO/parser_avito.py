1dad34cc567a2422db8d8de53663bd37c7587b93
be nice to me <46555349+AlcoholicAndMoron@users.noreply.github.com>
2022-12-12 20:28:21 +0300

    Update parser_avito.py

--- AVITO/parser_avito.py
+++ AVITO/parser_avito.py
@@ -26,20 +26,25 @@ def get_last(url):
     
     return last
 #########################################################################################################################################################  
+avito = 'www.avito.ru'
 
-url = 'https://www.avito.ru/all/noutbuki?cd=1&p=1&q=macbook'
-url1 = 'www.avito.ru'
+n = get_last(url)
+names = []
+links = []
+prices = []
 
-driver.get(url)
-time.sleep(2)
-html = driver.page_source
+for i in range(1, n): 
+    url = f'https://www.avito.ru/all/noutbuki?cd=1&p={i}&q=macbook'
+    driver.get(url)
+    time.sleep(2)
+    html = driver.page_source
+    
+    soup = BeautifulSoup(html, 'lxml')
+    names_temp = soup.find_all(class_='title-root-zZCwT iva-item-title-py3i_ title-listRedesign-_rejR title-root_maxHeight-X6PsH text-text-LurtD text-size-s-BxGpL text-bold-SinUO')
+    names += [i.text for i in names_temp]
+    
+    refs = soup.find_all(class_='iva-item-titleStep-pdebR')
+    links += [avito + i.find('a').get('href') for i in refs]
 
-soup = BeautifulSoup(html, 'lxml')
-models = soup.find_all(class_='title-root-zZCwT iva-item-title-py3i_ title-listRedesign-_rejR title-root_maxHeight-X6PsH text-text-LurtD text-size-s-BxGpL text-bold-SinUO')
-names += [i.text for i in headers]
-
-refs = soup.find_all(class_='iva-item-titleStep-pdebR')
-links = [url1 + i.find('a').get('href') for i in refs]
-
-prices = soup.find_all(attrs={'itemprop':'price'})
-prices = [i.get('content') for i in prices]
+    prices_temp = soup.find_all(attrs={'itemprop':'price'})
+    prices += [i.get('content') for i in prices_temp]
