da2068b09844386dd6d0ab1799f253130821315b
Axxolotl <103041459+Axxolotl@users.noreply.github.com>
2022-12-12 19:32:18 +0300

    Create parser_avito.py

---
+++ AVITO/parser_avito.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome.options import Options
+
+from bs4 import BeautifulSoup
+import requests
+import time
+import json
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+
+url = 'https://www.avito.ru/all/noutbuki?cd=1&p=1&q=macbook'
+
+driver.get(url)
+time.sleep(2)
+html = driver.page_source
