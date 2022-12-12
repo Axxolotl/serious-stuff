c29689e8d9c753dde5034d4cd3bda08cfcf938e5
be nice to me <46555349+AlcoholicAndMoron@users.noreply.github.com>
2022-12-12 20:03:44 +0300

    Update parser_avito.py

--- AVITO/parser_avito.py
+++ AVITO/parser_avito.py
@@ -21,5 +21,8 @@ soup = BeautifulSoup(html, 'lxml')
 models = soup.find_all(class_='title-root-zZCwT iva-item-title-py3i_ title-listRedesign-_rejR title-root_maxHeight-X6PsH text-text-LurtD text-size-s-BxGpL text-bold-SinUO')
 models = [i.text for i in headers]
 
+refs = soup.find_all(class_='iva-item-titleStep-pdebR')
+links = [url1 + i.find('a').get('href') for i in refs]
+
 prices = soup.find_all(attrs={'itemprop':'price'})
 prices = [i.get('content') for i in prices]
