546581b18e76984c7fe01d1ee9f154e243907510
Axxolotl <103041459+Axxolotl@users.noreply.github.com>
2022-12-12 20:15:26 +0300

    Update parser_avito.py

--- AVITO/parser_avito.py
+++ AVITO/parser_avito.py
@@ -7,9 +7,25 @@ from bs4 import BeautifulSoup
 import time
 import json
 
+# создание драйвера селениума
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+
+############################################################## ФУНКЦИЯ ПОЛУЧЕНИЯ НОМЕРА ПОСЛЕДНЕЙ СТРАНИЦЫ ##############################################
+url = 'https://www.avito.ru/all/noutbuki?cd=1&p=1&q=macbook'
+
+def get_last(url):
+    driver.get(url)
+    time.sleep(2)
+    
+    html = driver.page_source
+    
+    soup = BeautifulSoup(html, 'lxml')
+    last = int(soup.find_all(class_='pagination-item-JJq_j')[-2].text)
+    
+    return last
+#########################################################################################################################################################  
 
 url = 'https://www.avito.ru/all/noutbuki?cd=1&p=1&q=macbook'
 url1 = 'www.avito.ru'
@@ -20,7 +36,7 @@ html = driver.page_source
 
 soup = BeautifulSoup(html, 'lxml')
 models = soup.find_all(class_='title-root-zZCwT iva-item-title-py3i_ title-listRedesign-_rejR title-root_maxHeight-X6PsH text-text-LurtD text-size-s-BxGpL text-bold-SinUO')
-models = [i.text for i in headers]
+names += [i.text for i in headers]
 
 refs = soup.find_all(class_='iva-item-titleStep-pdebR')
 links = [url1 + i.find('a').get('href') for i in refs]
