33d5b793a9f2fcb4356d5c1b189dde6723892c6a
be nice to me <46555349+AlcoholicAndMoron@users.noreply.github.com>
2022-12-17 16:51:27 +0300

    Update wildberries.py

--- wildberries_cringe/wildberries.py
+++ wildberries_cringe/wildberries.py
@@ -11,3 +11,9 @@ def start(message):
     text = '''Привет!\n Рада, что ты решил(а) провести четверг с пользой и сделал(а) первый шаг на пути к новой жизни.\n\nДля регистрации на открытый вебинар в zoom «Как новичку или действующему поставщику выйти на миллион без рисков» остался один шаг:\n\nНапиши <b> корректно свой номер телефона </b> в ответ на это сообщение.\n\nСразу после тебе придёт ссылка на канал вебинара.
             '''
     bot.send_photo(message.chat.id, open(r'photoswb\Frame 47 (1).png', 'rb'), caption=text, parse_mode='HTML')
+    phone(message)
+    
+def phone (message):
+    keyboard = types.ReplyKeyboardMarkup(row_width = 1, resize_keyboard = True, one_time_keyboard=True)
+    button_phone = types.KeyboardButton(text = "Отправить номер", request_contact = True)
+    keyboard.add(button_phone)
