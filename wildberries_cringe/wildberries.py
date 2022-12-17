ad7da60fbd670681b3866806d850b0dbd472ab1a
Axxolotl <103041459+Axxolotl@users.noreply.github.com>
2022-12-17 16:49:35 +0300

    Update wildberries.py

--- wildberries_cringe/wildberries.py
+++ wildberries_cringe/wildberries.py
@@ -10,4 +10,4 @@ bot = telebot.TeleBot(token=token, parse_mode=None)
 def start(message):
     text = '''Привет!\n Рада, что ты решил(а) провести четверг с пользой и сделал(а) первый шаг на пути к новой жизни.\n\nДля регистрации на открытый вебинар в zoom «Как новичку или действующему поставщику выйти на миллион без рисков» остался один шаг:\n\nНапиши <b> корректно свой номер телефона </b> в ответ на это сообщение.\n\nСразу после тебе придёт ссылка на канал вебинара.
             '''
-    bot.send_photo(message.chat.id, open(r'C:\Users\petti\Downloads\Frame 47 (1).png', 'rb'), caption=text, parse_mode='HTML')
+    bot.send_photo(message.chat.id, open(r'photoswb\Frame 47 (1).png', 'rb'), caption=text, parse_mode='HTML')
